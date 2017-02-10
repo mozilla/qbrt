84da38debd0635601aabb4d0a21f638ce63458cf
Myk Melez <myk@mykzilla.org>
2017-02-10 14:45:59 -0800

    disable pref to that triggers exception

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,10 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("browser.dom.window.dump.enabled", true);
-pref("dom.mozBrowserFramesEnabled", true);
-pref("javascript.options.showInConsole", true);
-pref("devtools.selfxss.count", 5);
-pref("devtools.debugger.remote-enabled", true);
+// App-specific preferences, in alphabetical order.
+
+// Disabled, because for some reason it dumps:
+// JavaScript error: resource://gre/components/nsPrompter.js, line 350: NS_ERROR_NOT_AVAILABLE: Cannot call openModalWindow on a hidden window
+// pref("browser.dom.window.dump.enabled", true);
+
 pref("devtools.chrome.enabled", true);
 pref("devtools.debugger.prompt-connection", false);
+pref("devtools.debugger.remote-enabled", true);
+pref("devtools.selfxss.count", 5);
+pref("dom.mozBrowserFramesEnabled", true);
+pref("javascript.options.showInConsole", true);
