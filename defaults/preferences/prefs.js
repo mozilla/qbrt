28b497708a889380c0f6a667dae6357ae4691a63
Myk Melez <myk@mykzilla.org>
2017-02-10 13:51:57 -0800

    add default prefs

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+pref("browser.dom.window.dump.enabled", true);
+pref("dom.mozBrowserFramesEnabled", true);
+pref("javascript.options.showInConsole", true);
+pref("devtools.selfxss.count", 5);
+pref("devtools.debugger.remote-enabled", true);
+pref("devtools.chrome.enabled", true);
+pref("devtools.debugger.prompt-connection", false);
