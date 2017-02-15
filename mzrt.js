8751a2f370e25a9c84ca1de3396c87084a645199
Myk Melez <myk@mykzilla.org>
2017-02-15 13:58:16 -0800

    add command parser

--- mzrt.js
+++ mzrt.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+const commandLineCommands = require('command-line-commands');
+
+const validCommands = [ null ];
+const { command, argv } = commandLineCommands(validCommands);
+
+console.log('command: %s', command);
+console.log('argv:    %s', JSON.stringify(argv));
