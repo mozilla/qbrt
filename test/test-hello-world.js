7d5aa4e349df5764a571df427fb7fe2f5d240806
Myk Melez <myk@mykzilla.org>
2017-02-28 00:41:22 -0800

    log stdout and stderr data

--- test/test-hello-world.js
+++ test/test-hello-world.js
@@ -8,7 +8,13 @@ const spawn = require('child_process').spawn;
 const childProcess = spawn('node', [ path.join('bin', 'cli.js'), 'run', 'test/hello-world.js' ]);
 
 childProcess.stdout.on('data', data => {
-  assert.equal(data.toString('utf8'), 'console.log: Hello, World!\n');
+  const output = data.toString('utf8');
+  console.log(output);
+  assert.equal(output, 'console.log: Hello, World!\n');
+});
+
+childProcess.stderr.on('data', data => {
+  console.error(data.toString('utf8'));
 });
 
 childProcess.on('close', code => {
