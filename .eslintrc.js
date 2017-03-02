f9b645b73691011730a8466c15d1c64a765a87fa
Myk Melez <myk@mykzilla.org>
2017-03-02 15:45:08 -0800

    switch to eslint for moar control

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 8,
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
